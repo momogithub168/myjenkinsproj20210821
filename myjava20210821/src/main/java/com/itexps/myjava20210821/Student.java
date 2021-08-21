72221bc6d5e474c92eed130d6361605eb498d332
momogithub168 <86395463+momogithub168@users.noreply.github.com>
2021-08-21 10:29:09 -0500

    First Project

---
+++ myjava20210821/src/main/java/com/itexps/myjava20210821/Student.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itexps.myjava20210821;
+
+/**
+ *
+ * @author jmowa
+ */
+public class Student {
+
+    private String fName;
+    private String lName;
+
+    public Student(String fName, String lName) {
+        this.fName = fName;
+        this.lName = lName;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "fName=" + fName + ", lName=" + lName + '}';
+    }
+
+    
+}
---
+++ myjava20210821/src/test/java/test/com/StudentTest.java
@@ -0,0 +1,52 @@
+package test.com;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.itexps.myjava20210821.Student;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jmowa
+ */
+public class StudentTest {
+    Student stu;
+    
+    public StudentTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        stu = new Student("Smith", "White");
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testFirst() {
+    Assert.assertEquals("Smith", stu.getfName());
+    }
+}
